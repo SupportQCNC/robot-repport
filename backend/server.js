1a2e1d77da0df6012948b0e25c4f3b71d734541e
SupportQCNC <support@quincaillerie.nc>
2024-11-28 11:04:05 +1100

    modification eereur

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import usersRouter from './routes/users.js';
+import usersRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
